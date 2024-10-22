c1cdee9301209a7fecaf1bf68eb98fa14aaef4b7
Weseunghyun <bible091192@gmail.com>
2024-10-22 13:08:05 +0900

    feat: 랜덤 정수 생성 클래스 생성 1~9 까지 중복없이 세자리 수를 생성해주는 RandomNumGenerator의 generateRandomNum 메서드 생성

--- src/RandomNumGenerator.java
+++ src/RandomNumGenerator.java
@@ -1,4 +1,7 @@
+
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class RandomNumGenerator {
@@ -11,8 +14,14 @@ public class RandomNumGenerator {
             randomNumSet.add(randomNum);
         }
 
+        /*toList는 반환되는 리스트가 수정할 수 없는 리스트이다.
+        * shuffle은 수정가능한 리스트에서만 작동한다. 그냥 toList를 하고 shuffle을 호출하면
+        * UnspportedOperationException이 발생할 수 있다.*/
+        //List<Integer> randomNumList = randomNumSet.stream().toList();
+        List<Integer> randomNumList = new java.util.ArrayList<>(randomNumSet.stream().toList());
+        Collections.shuffle(randomNumList);
         StringBuilder randomNumBuilder = new StringBuilder();
-        for (int randomNum : randomNumSet) {
+        for (int randomNum : randomNumList) {
             randomNumBuilder.append(randomNum);
         }
 
